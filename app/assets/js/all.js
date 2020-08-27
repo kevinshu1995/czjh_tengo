0d91ce282731ff8c7690d1d56868cf723963809e
kevinHsu <kevinshu1995@gmail.com>
2020-08-28 00:48:51 +0800

    loading page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,7 @@
 console.log('不要來偷看');
+
+function loadingFinish() {
+    var loadingEl = document.getElementById('loading--hook')
+    loadingEl.style.display = 'none'
+}
+window.onload = loadingFinish;
